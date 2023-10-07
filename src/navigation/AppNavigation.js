be0b310b000db0d6acc2e9574ab67f89b0547866
BetjaderOD <20213tn032@utez.edu.mx>
2023-10-06 19:42:26 -0600

    Creacion de Nav

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,12 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useRoute } from "@react-navigation/native";
+import HomeScreen from '../screens/Home/HomeScreen';
 
-export default function AppNavigation() {
-  return (
-    <View>
-      <Text>AppNavigation</Text>
-    </View>
-  )
+
+
+const Stack =  createStackNavigator();
+
+export default class AppNavigation extends Component {
+  render() {
+    return (
+      <View>
+        <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+        </NavigationContainer>
+      </View>
+    )
+  }
 }
-
-const styles = StyleSheet.create({})
